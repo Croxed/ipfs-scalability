4057e0f0d262d792201f6d1bb7ab90da88ba1341
Oscar Wennergren <oscar@wennergren.se>
2018-02-22 13:19:15 +0100

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- run.sh
+++ run.sh
@@ -2,52 +2,68 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 HOST="http://localhost:8080/ipfs"
-# IPFS_HASH="QmVQzYSyg8UKESocmMjdzj1XofJDGHg59iK3QtRviL6pvo"
-IPFS_HASH="QmRQrVc93rq5JCGH4kPnxSBt8HbMbXQrEpkdP1dkiTfC6M"
+IPFS_HASH="QmUtyrtpwXy7fq6pu6rFQijNcmZaY6XeR2n3oThu2XjBEQ"
 STATS_FILE="stats.csv"
 touch $STATS_FILE
 sed -i "1s/.*/time,size,file,nodes/" "$STATS_FILE"
 
 SPEED="125M"    # Limit network speed for cURL
-NODES=(10 30 50)
+KBITSPEED=1048576 # 1Gbit in Kbit
+NODES=(10)
 for node in "${NODES[@]}"; do
+    Comcast --stop
+    
     iptb init -n "$node" --bootstrap none -f
+    trickled 
+    # iptb start --wait
+    for (( i = 0; i < "$node"; i++ )); do
+        export IPFS_PATH="$HOME/testbed/$i"
+        trickle -u "$KBITSPEED" -d "$KBITSPEED" ipfs daemon > "$IPFS_PATH/daemon.stdout" 2> "$IPFS_PATH/daemon.stderr" &
+        echo $! > "$IPFS_PATH/daemon.pid"
+        unset IPFS_PATH
+    done
     
-    export IPFS_PATH="$HOME/testbed/0"
-    ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080;
-    unset IPFS_PATH
-    
-    iptb start
+    sleep 10
     export IPFS_PATH="$HOME/testbed/0"
     NODE_0_ADDR="$(ipfs id -f \"\<addrs\>\" | head -n 1 | cut -c 2-)"
+    ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+    ipfs config Datastore.StorageMax 0GB
+    ipfs config Datastore.GCPeriod 0h
     unset IPFS_PATH
     echo "${NODE_0_ADDR}"
-    
-    find ~/testbed/* -maxdepth 0 -type d -exec bash -c "export IPFS_PATH=$1; ipfs bootstrap add ${NODE_0_ADDR}; unset IPFS_PATH" _ {} \;
    
     for (( i = 1; i < "$node"; i++ )); do
         export IPFS_PATH="$HOME/testbed/$i"
+        ipfs bootstrap add "${NODE_0_ADDR}"
         ipfs swarm connect "${NODE_0_ADDR}"
         unset IPFS_PATH
     done
-    export IPFS_PATH="$HOME/testbed/$(($node - 1))"
-    ipfs add -r "$DIR/files"
-    unset IPFS_PATH
-
-    IPFS_FILE="song.mp3"
-    ITERATIONS=40
-    for (( i = 0; i < "$ITERATIONS"; i++ )); do
-        curl --limit-rate "$SPEED" -sSn "$HOST/$IPFS_HASH/$IPFS_FILE" -o /dev/null -w "%{time_total},%{size_download}," >> $STATS_FILE
-        echo "$IPFS_FILE,$node" >> $STATS_FILE
+    
+    for (( i = (($node - 3)); i < "$node"; i++ )); do
+        export IPFS_PATH="$HOME/testbed/$i"
+        ipfs add -r "$DIR/files" &> /dev/null
+        unset IPFS_PATH
     done
 
-    # ITERATIONS=15
-    # IPFS_FILE="film.mp4"
-    # for (( i = 0; i < "$ITERATIONS"; i++ )); do
-    #     curl --limit-rate "$SPEED" -sSn "$HOST/$IPFS_HASH/$IPFS_FILE" -o /dev/null -w "%{time_total},%{size_download}," >> $STATS_FILE
-    #     echo "$IPFS_FILE,$node" >> $STATS_FILE
-    # done
+    Comcast --device=lo --latency=50
+    
+    export IPFS_PATH="$HOME/testbed/0"
+    IPFS_FILE="go.src"
+    rm -rf "$DIR/downloaded"
+    ITERATIONS=40
+    IPFS_FILE_SIZE="$(ipfs files stat "/ipfs/$IPFS_HASH" | awk 'FNR == 2 { print $2 }')"
+    for (( i = 0; i < "$ITERATIONS"; i++ )); do
+        curl --limit-rate "$SPEED" -sSn "$HOST/$IPFS_HASH" -o /dev/null -w "%{time_total},%{size_download}," >> $STATS_FILE
+        echo "$IPFS_FILE,$node" >> $STATS_FILE
+        ipfs repo gc &> /dev/null
+        rm -rf "$DIR/downloaded"
+    done
 
-    iptb stop
+    unset IPFS_PATH
+    pkill ipfs
+    pkill trickle
+    pkill trickled
+    Comcast --stop
+    # iptb stop
 done
-python3 "$DIR/graph_builder/grapher.py" "$DIR/stats.csv"
+python36 "$DIR/graph_builder/grapher.py" "$DIR/stats.csv"
