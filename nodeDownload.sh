ad16ee0e6bfbbb2c3d945a8f5278f9ba5f67d4e7
Oscar Wennergren <oscar@wennergren.se>
2018-03-06 14:30:22 +0100

    Now added delete for real

--- nodeDownload.sh
+++ nodeDownload.sh
@@ -10,7 +10,7 @@ KBITSPEED=1048576 # 1Gbit in Kbit
 export IPFS_PATH="$HOME/testbed/$HOST"
 for (( i = 0; i < "$ITERATIONS"; i++ )); do
     start_time="$(date +%s%3N | sed 's/N$//')"
-    trickle -s -u "$KBITSPEED" -d "$KBITSPEED" ipfs get "$IPFS_HASH" -a true -o /dev/null > /dev/null
+    trickle -s -u "$KBITSPEED" -d "$KBITSPEED" ipfs get "$IPFS_HASH" -a -o /dev/null > /dev/null
     end_time="$(date +%s%3N | sed 's/N$//')"
     milli_time="$(($end_time - $start_time))"
     time_secs=$(echo "scale=2;${milli_time}/1000" | bc)
