1d40760f043414ebfe9f8313b1d6920f94152f9b
Oscar Wennergren <oscar@wennergren.se>
2018-03-09 09:19:48 +0100

    Something fixed

--- download.sh
+++ download.sh
@@ -5,13 +5,13 @@ IPFS_FILE_SIZE=$3
 IPFS_FILE=$4
 node=$5
 ITERATIONS=$6
-API=$7
-CLIENTS=$8
+# API=$7
+CLIENTS=$7
 SPEED="12M"
 TIME=10
 
 for (( i = 0; i < "$ITERATIONS"; i++ )); do
     curl_data="$(curl --connect-timeout $TIME --max-time $TIME --limit-rate $SPEED -sSn "$HOST/$IPFS_HASH" -o /dev/null -w "%{time_total}")"
     echo "$curl_data,$IPFS_FILE_SIZE,$IPFS_FILE,$node,$CLIENTS"
-    curl -sSn "$API/repo/gc" &> /dev/null
+    # curl -sSn "$API/repo/gc" &> /dev/null
 done
