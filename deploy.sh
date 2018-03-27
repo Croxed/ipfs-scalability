90bbcf50f191abe06bef1ccb5dad260a05b7231c
Oscar Wennergren <oscar@wennergren.se>
2018-03-27 13:02:03 +0200

    Push poorly written test can down the road another ten years

--- deploy.sh
+++ deploy.sh
@@ -71,7 +71,7 @@ echo "Done bootstrapping $((NODE)) nodes.."
 NODE_0_ADDR="$(curl -s http://localhost:5001/api/v0/id?format=\<id\> | jq '.Addresses[0]' | cut -d "\"" -f 2 | sed "s/127.0.0.1/${MYIP}/")"
 IFS=' ' read -r -a array <<< "$@"
 for cluster in "${array[@]}" ; do
-    ssh -n -f root@"$cluster" bash -c "'(cd /root/ipfs-scalability; git reset HEAD --hard; git pull; nohup bash /root/ipfs-scalability/deploy_cluster.sh $NODE_0_ADDR $NODES > /root/ipfs-scalability/daemon.out 2>&1) &'"
+    ssh -n -f root@"$cluster" bash -c "'(cd /root/ipfs-scalability; hohup bash /root/ipfs-scalability/deploy_cluster.sh $NODE_0_ADDR $NODES > /root/ipfs-scalability/daemon.out 2>&1) &'"
 done
 tc qdisc add dev "$DEV" root netem delay "$DELAY" 20ms distribution normal
 tc qdisc add dev "$DEV1" root netem delay "$DELAY" 20ms distribution normal
