8195e70b7223857dca48ae46f8f598efe9d1d988
Oscar Wennergren <oscar@wennergren.se>
2018-03-29 23:32:32 +0200

    Updated python script for downloading

--- node_download.py
+++ node_download.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """ Script to run simulation tests on IPFS using ipfsapi and python """
 
-import glob
 import os
-import random
-import re
-import sys
 import subprocess
-from contextlib import contextmanager
+import sys
 import time
+from contextlib import contextmanager
+from threading import Thread
+from urllib.parse import urlparse
+
 import ipfsapi
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file = open(dir_path + "/stats.csv", "a")
-nodes = sys.argv[4]
+nodes = sys.argv[4:]
 
 
 @contextmanager
@@ -39,13 +39,26 @@ def subprocess_cmd(command):
     file.write(time_string)
 
 
-def scalability_test(IPFS_PATH, HASH, iterations):
+def scalability_test(ipfs_path, ipfs_hash, iterations):
     """ Main method for scalability test """
-    os.environ["IPFS_PATH"] = IPFS_PATH
-    for i in range(0, int(iterations)):
-        subprocess_cmd("ipfs cat /ipfs/%s &> /dev/null" % HASH)
+    with suppress_stdout():
+        for node in nodes:
+            node_url = urlparse(node)  # Parses the given URL
+            ipfs_node = ipfsapi.connect(node_url.hostname, node_url.port)
+            Thread(
+                target=ipfs_node.add(
+                    dir_path + '/files/go-ipfs-0.4.13',
+                    recursive=True)).start()
+        # os.environ["IPFS_PATH"] = ipfs_path
+        gateway_node = ipfsapi.connect()
+        for _ in range(0, int(iterations)):
+            # subprocess_cmd("ipfs cat /ipfs/%s &> /dev/null" % ipfs_hash)
+            start_time = time.time()
+            gateway_node.get(ipfs_hash)
+            time_string = str(time.time() - start_time) + "," + nodes + '\n'
+            file.write(time_string)
+            subprocess_cmd("rm -rf %s/go-ipfs-0.4.13" % dir_path)
 
 
 if __name__ == '__main__':
-    file.write("time,nodes\n")
     scalability_test(sys.argv[1], sys.argv[2], sys.argv[3])
