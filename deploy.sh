c7b91f33682eef885caf0e61f2824bdb88e402c1
Oscar Wennergren <oscar@wennergren.se>
2018-04-03 12:40:04 +0200

    Yeah, sure, because people just order stuff from Amazon and forget what they got.

--- deploy.sh
+++ deploy.sh
@@ -14,66 +14,63 @@ CLIENTS=1
 # NODES=32
 CLUSTER_NODES=(16 32 64)
 MYIP="$(ip add | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.*')"
-for NODES in "${CLUSTER_NODES[@]}"; do
-	tc qdisc del dev "$DEV" root netem
-	tc qdisc del dev "$DEV1" root netem
-	umount /ipfs
-	umount /ipns
-	rm -rf "$DIR/deploy/ipfs*"
-    rm -rf "$DIR/clients_*"
-	APIPORT=5001
-	APILIST=()
-	pids=()
-	rm -rf "$DIR/deploy/ipfs0"
-	mkdir -p "$DIR/deploy/ipfs0"
-	export IPFS_PATH="$DIR/deploy/ipfs0"
-	ipfs init -e --profile test &>/dev/null &
-	pids+=($!)
-	ipfs bootstrap rm all &>/dev/null &
-	APILIST+=($((APIPORT + i)))
-	unset IPFS_PATH
-	wait "${pids[@]}"
+tc qdisc del dev "$DEV" root netem
+tc qdisc del dev "$DEV1" root netem
+umount /ipfs
+umount /ipns
+rm -rf "$DIR/deploy/ipfs*"
+APIPORT=5001
+APILIST=()
+pids=()
+rm -rf "$DIR/deploy/ipfs0"
+mkdir -p "$DIR/deploy/ipfs0"
+export IPFS_PATH="$DIR/deploy/ipfs0"
+ipfs init -e --profile test &>/dev/null &
+pids+=($!)
+ipfs bootstrap rm all &>/dev/null &
+APILIST+=($((APIPORT + i)))
+unset IPFS_PATH
+wait "${pids[@]}"
 
-	export IPFS_PATH="$DIR/deploy/ipfs0"
-	ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-	ipfs config Datastore.StorageMax 0GB
-	ipfs config --json Datastore.StorageGCWatermark 0
-	ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
-	ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"
-	ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
-	ipfs config Datastore.GCPeriod 0h
-	ipfs config Addresses.API /ip4/0.0.0.0/tcp/"$((APIPORT + i))"
-	APILIST+=($((APIPORT + i)))
-	sed -i 's/127.0.0.1/0.0.0.0/g' "$IPFS_PATH/config"
-	trickle -s -u "$KBITSPEED" -d "$KBITSPEED" ipfs daemon --enable-gc=true >"$IPFS_PATH/daemon.stdout" 2>"$IPFS_PATH/daemon.stderr" &
-	echo $! >"$IPFS_PATH/daemon.pid"
-	echo "Starting node $i"
-	unset IPFS_PATH
+export IPFS_PATH="$DIR/deploy/ipfs0"
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Datastore.StorageMax 0GB
+ipfs config --json Datastore.StorageGCWatermark 0
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
+ipfs config Datastore.GCPeriod 0h
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/"$((APIPORT + i))"
+APILIST+=($((APIPORT + i)))
+sed -i 's/127.0.0.1/0.0.0.0/g' "$IPFS_PATH/config"
+trickle -s -u "$KBITSPEED" -d "$KBITSPEED" ipfs daemon --enable-gc=true >"$IPFS_PATH/daemon.stdout" 2>"$IPFS_PATH/daemon.stderr" &
+echo $! >"$IPFS_PATH/daemon.pid"
+echo "Starting node $i"
+unset IPFS_PATH
 
+STARTED=0
+while ((STARTED < CLIENTS)); do
 	STARTED=0
-	while ((STARTED < CLIENTS)); do
-		STARTED=0
-		for requsts in "${APILIST[@]}"; do
-			if ! curl -fs "http://localhost:$requsts"; then
-				((STARTED++))
-			fi
-		done
-		sleep 1
+	for requsts in "${APILIST[@]}"; do
+		if ! curl -fs "http://localhost:$requsts"; then
+			((STARTED++))
+		fi
 	done
-	echo "Done starting daemons"
-	NODE_0_ADDR="$(curl -s http://localhost:5001/api/v0/id?format=\<id\> | jq '.Addresses[0]' | cut -d "\"" -f 2)"
-	export IPFS_PATH="$DIR/deploy/ipfs0"
-	IPFS_HASH="$(ipfs add -nr "$DIR/files/go-ipfs-0.4.13" | tail -n 1 | awk '{print $2}')"
-	unset IPFS_PATH
+	sleep 1
+done
+echo "Done starting daemons"
+NODE_0_ADDR="$(curl -s http://localhost:5001/api/v0/id?format=\<id\> | jq '.Addresses[0]' | cut -d "\"" -f 2)"
 
-	for ((i = 0; i < CLIENTS; i++)); do
-		API="http://localhost:$((APIPORT + i))/api/v0"
-		curl -sSn "$API/bootstrap/add?arg=${NODE_0_ADDR}" &>/dev/null
-		curl -sSn "$API/swarm/connect?arg=${NODE_0_ADDR}" &>/dev/null
-	done
-	echo "Done bootstrapping $((CLIENTS)) clients.."
-	NODE_0_ADDR="$(curl -s http://localhost:5001/api/v0/id?format=\<id\> | jq '.Addresses[0]' | cut -d "\"" -f 2 | sed "s/127.0.0.1/${MYIP}/")"
-	IFS=' ' read -r -a array <<<"$@"
+for ((i = 0; i < CLIENTS; i++)); do
+	API="http://localhost:$((APIPORT + i))/api/v0"
+	curl -sSn "$API/bootstrap/add?arg=${NODE_0_ADDR}" &>/dev/null
+	curl -sSn "$API/swarm/connect?arg=${NODE_0_ADDR}" &>/dev/null
+done
+echo "Done bootstrapping $((CLIENTS)) clients.."
+NODE_0_ADDR="$(curl -s http://localhost:5001/api/v0/id?format=\<id\> | jq '.Addresses[0]' | cut -d "\"" -f 2 | sed "s/127.0.0.1/${MYIP}/")"
+IFS=' ' read -r -a array <<<"$@"
+for NODES in "${CLUSTER_NODES[@]}"; do
+	rm -rf "$DIR/clients_*"
 	for cluster in "${array[@]}"; do
 		ssh -n -f root@"$cluster" bash -c "'(cd /root/ipfs-scalability; nohup bash /root/ipfs-scalability/deploy_cluster.sh $NODE_0_ADDR $NODES > /root/ipfs-scalability/daemon.out 2>&1 & echo $! > /root/ipfs-scalability/daemon.pid) &'"
 	done
@@ -89,8 +86,8 @@ for NODES in "${CLUSTER_NODES[@]}"; do
 	done
 
 	IPFS_FILE="$(find $DIR/files/* -maxdepth 0 -type d -exec basename {} \;)"
-    IPFS_FILE_SIZE="$(du -sh "$DIR/files/go-ipfs-0.4.13" | awk '{ print $1 }')"
-	ITERATIONS=300
+	IPFS_FILE_SIZE="$(du -sh "$DIR/files/go-ipfs-0.4.13" | awk '{ print $1 }')"
+	ITERATIONS=1000
 	pids=()
 	WEBPORT=8080
 	APIPORT=5001
@@ -98,5 +95,5 @@ for NODES in "${CLUSTER_NODES[@]}"; do
 	HOST="http://localhost:$((WEBPORT))/ipfs"
 	API="http://localhost:$((APIPORT))/api/v0"
 	echo "python3 "$DIR/node_download.py" $((${#array[@]} * NODES)) $ITERATIONS $IPFS_FILE $IPFS_FILE_SIZE"
-	python3 "$DIR/node_download.py" "$((${#array[@]} * NODES))" "$ITERATIONS" "$IPFS_FILE" "$IPFS_FILE_SIZE" 
+	python3 "$DIR/node_download.py" "$((${#array[@]} * NODES))" "$ITERATIONS" "$IPFS_FILE" "$IPFS_FILE_SIZE"
 done
