761229fb6c8879e45292cccc15a316a857305b96
Oscar Wennergren <oscar@wennergren.se>
2018-04-04 09:33:21 +0200

    syntax

--- node_download.py
+++ node_download.py
@@ -77,6 +77,7 @@ def upload_files(node, q):
 def download_files(node, iterations, gateway, ipfs_hash, file_size, file,
                    nr_nodes):
     """ Download the given file from IPFS """
+    print("Downloading {} times from node {} ".format(iterations, node))
     move_path = os.path.join(dir_path, "newDir_{}".format(node))
     subprocess_cmd("rm -rf {}".format(move_path))
     for _ in range(0, int(iterations)):
